4e7a9975060df3f805207626e14595af12b8196f
Leticia Tomasio <tp.leticia.tomasio@totvspartners.com.br>
2021-12-30 17:39:59 -0300

    implements home page and subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,6 +9,18 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
+      <main className={styles.mainContent}>
+        <div className={styles.textContainer}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span className={styles.cyanText}>React</span> world
+          </h1>
+          <p>Get access to all the publications</p>
+          <p className={styles.cyanText}>for $9.90 month</p>
+          <SubscribeButton />
+        </div>
+        <img src="/images/illustration.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
