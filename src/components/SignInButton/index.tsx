1321a4517638eaf3452d928c5373a501c24113ca
Leticia Tomasio <tp.leticia.tomasio@totvspartners.com.br>
2022-01-01 20:54:35 -0300

    implements authentication with next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,28 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isLoggedIn = false;
+  const { data: session } = useSession();
 
-  return isLoggedIn ? (
+  return session ? (
     <button type="button" className={styles.btn}>
       <FaGithub color="#04D361" />
-      <span>user</span>
-      <FiX color="#737380" className={styles.closeIcon} />
+      <span>{session.user.name}</span>
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.btn}>
+    <button
+      type="button"
+      className={styles.btn}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       <span>Sign in with Github</span>
     </button>
